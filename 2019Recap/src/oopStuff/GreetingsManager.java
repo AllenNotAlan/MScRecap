1c9fe99321f09fc589d5cd50b8e9dc59469fdd41
allen <allen@DESKTOP-ORSIH3L.Home>
2019-10-22 00:13:37 +0100

    question 9 updated and oopStuff added

---
+++ 2019Recap/src/oopStuff/GreetingsManager.java
@@ -0,0 +1,25 @@
+package oopStuff;
+
+/**
+ * 
+ * @author allen
+ *
+ */
+public class GreetingsManager {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Greetings Manager App ..... start");
+		
+		//create object of type MessageCentre
+		MessageCentre myMessageCentre = new MessageCentre();
+		myMessageCentre.displayWelcomeMessage();
+		
+		System.out.println("Greetings Manager App ..... close");
+		
+	}
+	
+}
---
+++ 2019Recap/src/oopStuff/MessageCentre.java
@@ -0,0 +1,17 @@
+package oopStuff;
+
+/**
+ * Class to respond with appropriate messages on method call
+ * @author allen
+ *
+ */
+public class MessageCentre {
+	
+	/**
+	 * Method that prints to screen a welcome message
+	 * 
+	 */
+	public void displayWelcomeMessage() {
+		System.out.println("Hello and welcome");
+	}//end of method
+}// end of class
